6a45ef65e65f20c9196ee3cfbd26aa925b070789
matheuskvn <matheus.kbm@gmail.com>
2025-07-21 06:58:10 +0200

    Creating a email client using Nodemailer

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import nodemailer from 'nodemailer';
+import 'dotenv/config';
+
+const PORT = process.env.PORT || 3000;
+const EMAIL = process.env.EMAIL;
+const PASSWORD = process.env.PASSWORD;
+
+const app = express();
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: { user: EMAIL, pass: PASSWORD }
+});
+
+app.get('/send', async (req, res) => {
+    try {
+        await transporter.sendMail({
+            to: 'your_email@gmail.com',
+            subject: 'Node.js test',
+            text: 'E-mail sent via Nodemailer!'
+        });
+        res.send('E-mail sent!');
+    } catch (error) {
+        res.status(500).send('Erro ao enviar o email: ' + error.message);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
